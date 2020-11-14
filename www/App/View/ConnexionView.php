a6af36c4fdf75e02fb96826d0eae231b4f8ad413
= <=>
2020-11-14 22:34:28 +0100

    adding some style for connexion form

--- www/App/View/ConnexionView.php
+++ www/App/View/ConnexionView.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;500&display=swap" rel="stylesheet">
     
 </head>
 <body>
     <div class="wrapper">
         <h1>Ecran de connexion</h1>
-        <form action= <?php //echo ROOT."/app/Controller/ConnexionControler" ; ?> method="POST">
+        <form action= <?= ROOT."/app/Controller/ConnexionControler" ; ?> method="POST">
         <!-- mail -->
         <label for="mail">Votre mail</label>    
         <input type="mail" name="mail">
